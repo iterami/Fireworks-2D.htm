d4c65b0e63a58e032f108834cbb22b5ec3af3ca3
honzi <czechinthemail@gmail.com>
2017-08-01 22:38:33 -0700

    replaced for loop with core_group_modify()

--- js/fireworks-2d.js
+++ js/fireworks-2d.js
@@ -1,15 +1,20 @@
 'use strict';
 
 function draw_logic(){
-    for(var entity in core_entities){
-        canvas_buffer.fillStyle = core_entities[entity]['color'];
-        canvas_buffer.fillRect(
-          core_entities[entity]['x'],
-          core_entities[entity]['y'],
-          core_entities[entity]['width'],
-          core_entities[entity]['height']
-        );
-    }
+    core_group_modify({
+      'groups': [
+        'firework',
+      ],
+      'todo': function(entity){
+          canvas_buffer.fillStyle = core_entities[entity]['color'];
+          canvas_buffer.fillRect(
+            core_entities[entity]['x'],
+            core_entities[entity]['y'],
+            core_entities[entity]['width'],
+            core_entities[entity]['height']
+          );
+      },
+    });
 }
 
 function launch(args){
@@ -38,7 +43,7 @@ function launch(args){
         'y': args['y'],
       },
       'types': [
-        'fireworks',
+        'firework',
       ],
     });
 }
@@ -48,44 +53,49 @@ function logic(){
         launch();
     }
 
-    for(var entity in core_entities){
-        core_entities[entity]['x'] += core_entities[entity]['dx'];
-        core_entities[entity]['y'] += core_entities[entity]['dy'];
+    core_group_modify({
+      'groups': [
+        'firework',
+      ],
+      'todo': function(entity){
+          core_entities[entity]['x'] += core_entities[entity]['dx'];
+          core_entities[entity]['y'] += core_entities[entity]['dy'];
 
-        core_entities[entity]['dy'] += .02;
-        core_entities[entity]['dx'] *= .99;
+          core_entities[entity]['dy'] += .02;
+          core_entities[entity]['dx'] *= .99;
 
-        core_entities[entity]['timer'] -= 1;
-        if(core_entities[entity]['timer'] <= 0){
-            if(core_entities[entity]['children'] > 0){
-                var loop_counter = core_entities[entity]['children'] - 1;
-                do{
-                    launch({
-                      'children': 0,
-                      'dx': Math.random() * 3 - 1.5,
-                      'dy': Math.random() * 3 - 1.5,
-                      'timer': core_random_integer({
-                        'max': 90,
-                      }) + 40,
-                      'x': core_entities[entity]['x'],
-                      'y': core_entities[entity]['y'],
-                    });
-                }while(loop_counter--);
-            }
+          core_entities[entity]['timer'] -= 1;
+          if(core_entities[entity]['timer'] <= 0){
+              if(core_entities[entity]['children'] > 0){
+                  var loop_counter = core_entities[entity]['children'] - 1;
+                  do{
+                      launch({
+                        'children': 0,
+                        'dx': Math.random() * 3 - 1.5,
+                        'dy': Math.random() * 3 - 1.5,
+                        'timer': core_random_integer({
+                          'max': 90,
+                        }) + 40,
+                        'x': core_entities[entity]['x'],
+                        'y': core_entities[entity]['y'],
+                      });
+                  }while(loop_counter--);
+              }
 
-            core_entity_remove({
-              'entities': [
-                entity,
-              ],
-            });
-        }
-    }
+              core_entity_remove({
+                'entities': [
+                  entity,
+                ],
+              });
+          }
+      },
+    });
 }
 
 function repo_init(){
     core_repo_init({
       'entities': {
-        'fireworks': {
+        'firework': {
           'default': true,
           'properties': {
             'children': 10,
