c7dc2a12cdac5ad8fc658681b52c364a60f2d9bd
honzi <czechinthemail@gmail.com>
2017-10-15 11:33:55 -0700

    moved launch() to data.js

--- js/main.js
+++ js/main.js
@@ -21,37 +21,6 @@ function draw_logic(){
     });
 }
 
-function launch(args){
-    args = core_args({
-      'args': args,
-      'defaults': {
-        'children': 10,
-        'dx': Math.random() * 4 - 2,
-        'dy': -Math.random() * 2 - canvas_properties['height'] / 200,
-        'timer': core_random_integer({
-          'max': 200,
-        }) + 100,
-        'x': core_mouse['x'],
-        'y': canvas_properties['height'],
-      },
-    });
-
-    core_entity_create({
-      'properties': {
-        'children': args['children'],
-        'color': '#' + core_random_hex(),
-        'dx': args['dx'],
-        'dy': args['dy'],
-        'timer': args['timer'],
-        'x': args['x'],
-        'y': args['y'],
-      },
-      'types': [
-        'firework',
-      ],
-    });
-}
-
 function logic(){
     if(core_mouse['down']){
         launch();
